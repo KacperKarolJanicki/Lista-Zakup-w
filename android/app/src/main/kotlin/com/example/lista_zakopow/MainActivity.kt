81ff8a995ae076f41afd255c57303b425804298d
Kacper Janicki <kacperkaroljanicki@gmail.com>
2024-11-28 21:38:38 +0100

    [iOS]

---
+++ android/app/src/main/kotlin/com/example/lista_zakopow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lista_zakopow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/My_Price_Input.dart
@@ -0,0 +1,63 @@
+double price_1 = 0;
+double price_2 = 0;
+double price_3 = 0;
+double price_4 = 0;
+double price_5 = 0;
+double price_6 = 0;
+double price_7 = 0;
+double price_8 = 0;
+double price_9 = 0;
+double price_10 = 0;
+
+
+int stock_1 = 1;
+int stock_2 = 1;
+int stock_3 = 1;
+int stock_4 = 1;
+int stock_5 = 1;
+int stock_6 = 1;
+int stock_7 = 1;
+int stock_8 = 1;
+int stock_9 = 1;
+int stock_10 = 1;
+
+double final_price_1 = 0;
+double final_price_2 = 0;
+double final_price_3 = 0;
+double final_price_4 = 0;
+double final_price_5 = 0;
+double final_price_6 = 0;
+double final_price_7 = 0;
+double final_price_8 = 0;
+double final_price_9 = 0;
+double final_price_10 = 0;
+
+
+double final_final_price = 0;
+
+void Reset_Values() {
+
+  price_1 = 0;
+  price_2 = 0;
+  price_3 = 0;
+  price_4 = 0;
+  price_5 = 0;
+  price_6 = 0;
+  price_7 = 0;
+  price_8 = 0;
+  price_9 = 0;
+  price_10 = 0;
+
+
+  stock_1 = 1;
+  stock_2 = 1;
+  stock_3 = 1;
+  stock_4 = 1;
+  stock_5 = 1;
+  stock_6 = 1;
+  stock_7 = 1;
+  stock_8 = 1;
+  stock_9 = 1;
+  stock_10 = 1;
+
+}
